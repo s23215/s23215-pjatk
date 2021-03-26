20b234a04ddb046f1870c737a13a78f8abce1697
karmenix <80523647+s23215@users.noreply.github.com>
2021-03-26 13:16:03 +0100

    Zadanie 1/2/3

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, session
+
+class User:
+	def __init__(self, id, login, password):
+		self.id=id
+		self.login=login
+		self.password=password
+	def __repr__(self):
+		return f'<User:{self.login}'
+users=[]
+users.append(User(id=1,login='Krystian', password='Banan'))
+
 
 app = Flask(__name__)
+app.secret_key="abc"
+
+@app.route('/', methods=('GET', 'POST'))
+def login():
+	if request.method=='POST':
+		login=request.form['login']
+		password=request.form['pass']
+		user=[x for x in users if x.login==login][0]
+		if user and user.password==password:
+			session['user_id']=user.id
+			return render_template('index.html')
+		else:
+			return render_template('login.html', login=login, wrongpass='Wrong Password!')
+	return render_template('login.html')
+
 
 
 @app.route('/example', methods=('GET', 'POST'))
@@ -28,7 +54,3 @@ def example2():
 	return 'You said:{}'.format(request.form['firstname'])
 
 
-
-@app.route('/')
-def index():
-	return render_template('index.html')
